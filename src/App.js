11e2110273fa25740652727e2197842182d6a96a
Van Nguyen <npnv.vn1@gmail.com>
2021-11-08 17:15:56 +0700

    Update titile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Van Nguyen AZ</p>
+        <p>Van Nguyen</p>
       </header>
     </div>
   );
